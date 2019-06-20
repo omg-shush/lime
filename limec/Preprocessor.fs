25a1ac313df8fc9bad54dfdb3b2e9f669b60b9e3
omg-shush <>
2019-06-19 19:51:53 -0700

    Preprocessor recognizes strings (both single and double quotes) and doesn't look for comments in them

--- limec/Preprocessor.fs
+++ limec/Preprocessor.fs
@@ -27,24 +27,45 @@ module Preprocessor =
             let output, _, potentialTrailingComment, potentialTrailingString =
                 List.fold (fun (output: (CodePosition * char) Stack, prevChar: char, inComment: Comment, inString: StringLiteral) (pos: CodePosition, nextChar: char) ->
                     match prevChar, nextChar, inComment, inString with
+                    // Entering states
                     | '/', '/', NoComment, NoString ->
                         // Enter line comment, replacing the '/' which was mistakenly written before
                         Cons (Cons (output.Bottom, (pos, ' ')), (pos, ' ')), nextChar, LineComment, NoString
-                    | _, '\n', LineComment, NoString ->
-                        // Exit line comment, keeping the newline
-                        Cons (output, (pos, nextChar)), nextChar, NoComment, NoString
-                    | _, _, LineComment, NoString ->
-                        // Stay in line comment, replacing all characters with whitespace
-                        Cons (output, (pos, ' ')), nextChar, LineComment, NoString
                     | '/', '*', NoComment, NoString ->
                         // Enter block comment, replacing the '/' which was mistakenly written before
                         Cons (Cons (output.Bottom, (pos, ' ')), (pos, ' ')), nextChar, BlockComment pos, NoString
+                    | _, '\'', NoComment, NoString ->
+                        // Enter single quote string
+                        Cons (output, (pos, nextChar)), nextChar, NoComment, SingleQuoteString pos
+                    | _, '"', NoComment, NoString ->
+                        // Enter double quote string
+                        Cons (output, (pos, nextChar)), nextChar, NoComment, DoubleQuoteString pos
+
+                    // Exiting states
+                    | _, '\n', LineComment, NoString ->
+                        // Exit line comment, keeping the newline
+                        Cons (output, (pos, nextChar)), nextChar, NoComment, NoString
                     | '*', '/', BlockComment _, NoString ->
                         // Exit block comment
                         output, nextChar, NoComment, NoString
+                    | x, '\'', NoComment, SingleQuoteString _ when x <> '\\' ->
+                        // Exit single quote string
+                        Cons (output, (pos, nextChar)), nextChar, NoComment, NoString
+                    | x, '"', NoComment, DoubleQuoteString _ when x <> '\\' ->
+                        // Exit double quote string
+                        Cons (output, (pos, nextChar)), nextChar, NoComment, NoString
+
+                    // Maintaining states
+                    | _, _, LineComment, NoString ->
+                        // Stay in line comment, replacing all characters with whitespace
+                        Cons (output, (pos, ' ')), nextChar, LineComment, NoString
                     | _, _, BlockComment bc, NoString ->
                         // Stay in block comment, replacing all characters with whitespace
                         Cons (output, (pos, ' ')), nextChar, BlockComment bc, NoString
+                    | _, _, NoComment, SingleQuoteString _
+                    | _, _, NoComment, DoubleQuoteString _ ->
+                        // Stay in string
+                        Cons (output, (pos, nextChar)), nextChar, NoComment, inString // Stay in same type of string though
                     | _ -> Cons (output, (pos, nextChar)), nextChar, inComment, inString // TODO
                 ) (Stack.Empty, ' ', NoComment, NoString) (Stack.makeList input)
             output
